56bb69575790dddba1be37c84e956c2085cabc61
romuloromulo <neoromulo@gmail.com>
2023-10-14 00:21:11 -0300

    att

--- app/programacao/page.js
+++ app/programacao/page.js
@@ -2,22 +2,39 @@ import React from "react";
 import Image from "next/image";
 import MainLayout from "../layout/MainLayout";
 import { programaçãoBarreirinhas, programaçãoSlz } from "@/data/FestivalData";
-import BaseEsquerda from "../../public/images/logos/Base-Esquerda.png";
+import BaseDireita from "../../public/images/logos/Base-Direita-Horizontal.png";
+import BaseEsquerda from "../../public/images/logos/Base-Esquerda-Horizontal.png";
+import BaseEsquerdaVertical from "../../public/images/logos/Base-Esquerda.png";
 
 import { BiDownArrow } from "react-icons/bi";
 
 function Programação() {
   return (
     <MainLayout>
-      <div id="Programação" className="min-h-screen relative">
+      <div id="Programação" className="min-h-screen relative pt-24">
         <div className="container mx-auto w-full flex items-start justify-between lg:pr-12 flex-col lg:flex-row ">
+          <div className="">
+            <Image
+              src={BaseDireita}
+              width={200}
+              height={200}
+              className="right-0 top-0 absolute md:mt-[75px] w-[170px]  h-[170px] md:w-auto md:h-auto"
+            />
+          </div>
+          <div>
+            <Image
+              src={BaseEsquerda}
+              width={200}
+              height={200}
+              className="left-0 top-0 absolute md:mt-[75px] w-[170px]  h-[170px] md:w-auto md:h-auto"
+            />
+          </div>
+
           <div className="w-full mt-[75px] flex md:flex-row flex-col items-center justify-between gap-4">
             <div className="relative p-12 md:pr-0 z-20">
-              <span className="absolute p-10 md:p-14 rounded-full bg-red-500 -z-40 top-[25%]  md:top-[15%] "></span>
-              <span className="absolute p-6 md:p-8 rounded-full bg-yellow-500 -z-40 right-[25%] top-[35%] md:right-[15%] "></span>
-              <div
-                className="text-5xl md:text-6xl font-bold text-zinc-100 z-20"
-                style={{ whiteSpace: "nowrap" }}>
+              {/* <span className="absolute p-10 md:p-14 rounded-full bg-red-500 -z-40 top-[25%]  md:top-[15%] "></span>
+              <span className="absolute p-6 md:p-8 rounded-full bg-yellow-500 -z-40 right-[25%] top-[35%] md:right-[15%] "></span> */}
+              <div className="text-4xl text-center md:text-left md:text-6xl font-bold text-zinc-100 z-20 md:flex-wrap">
                 {" "}
                 Programação 2023
               </div>
@@ -36,7 +53,7 @@ function Programação() {
               2 a 4 de Novembro, <br className="hidden md:block" />
               Praça da Matriz
             </p>
-            <p className="text-zinc-800 md:hidden mt-4">
+            <p className="text-zinc-100 md:hidden mt-4">
               <BiDownArrow size={22} />
             </p>
           </div>
@@ -68,21 +85,45 @@ function Programação() {
           </div>
         </div>
         <Image
-          src={BaseEsquerda}
+          src={BaseEsquerdaVertical}
           width={200}
           height={200}
           className="left-0 botton-0 mt-[-15%] hidden md:block"
         />
         <div
           id="São Luís"
-          className="w-full  min-h-screen bg-zinc-100 relativem pt-[75px]">
+          className="w-full  min-h-screen bg-zinc-100 relative pt-[75px]">
+          <div>
+            <div className="">
+              <Image
+                src={BaseDireita}
+                width={200}
+                height={200}
+                className="right-0 top-0 absolute md:mt-[75px] w-[170px]  h-[170px] md:w-auto md:h-auto md:hiddenh"
+              />
+            </div>
+            <div>
+              <Image
+                src={BaseEsquerda}
+                width={200}
+                height={200}
+                className="left-0 top-0 absolute md:mt-[75px] w-[170px]  h-[170px] md:w-auto md:h-auto md:hidden"
+              />
+            </div>
+            <Image
+              src={BaseDireita}
+              width={200}
+              height={200}
+              className="right-0 top-0 hidden md:block absolute w-[120px]  h-[120px] md:w-auto md:h-auto"
+            />
+          </div>
           <div className="container mx-auto  flex md:flex-row flex-col  items-center  lg:px-12 text-zinc-900 py-20 ">
             <div className="flex-col md:w-auto w-full flex items-center md:items-start justify-center md:justify-start border-white md:border-r md:border-red-300 md:pr-12">
               <h1 className="text-5xl font-semibold font-sans ">São Luís</h1>
               <p className="text-md sm:text-lg font-semibold">
                 10 a 12 de Novembro, Convento das Mercês
               </p>
-              <p className="text-white md:hidden mt-4">
+              <p className="text-zinc-900 md:hidden mt-4">
                 <BiDownArrow size={22} />
               </p>
             </div>
@@ -116,7 +157,7 @@ function Programação() {
             </div>
           </div>
           <Image
-            src={BaseEsquerda}
+            src={BaseEsquerdaVertical}
             width={200}
             height={200}
             className="left-0 botton-0 mt-[-15%] hidden md:block"
